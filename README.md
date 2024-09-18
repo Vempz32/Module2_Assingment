146d7d27e074e3951e827dcb89b213a8c804d9db
Vempz32 <Tyjablonski@icloud.com>
2024-09-18 10:27:34 -0500

    Initial commit: Set up project directory and file structure

---
+++ README.md
@@ -0,0 +1,13 @@
+# [Project Name]
+Module-2 Assingment
+## Description
+
+Module-2 Assingment
+
+## Author
+
+Tyler Jablonski
+
+## Assignment
+
+Module-2  Assingment Data Types
---
+++ sdf_assignment_2.py
@@ -0,0 +1,18 @@
+"""
+Description: Module 2 Assingment Data Types
+Author:Tyler Jablonski
+Date: Sept 18 2024
+"""
+
+#SIMPLE DATA TYPES
+
+#CALCULATIONS
+
+#LISTS
+
+#TUPLES
+
+#DICTIONARIES
+
+#SETS
+
